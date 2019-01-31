e854fddb601c30ca4557e479a5f6424ff0f0e6cb
kiranpuppala_juspay <kiran.puppala@juspay.in>
2019-01-31 22:41:36 +0530

    ..

---
+++ Assets/Scripts/SpinTop.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinTop : MonoBehaviour {
+
+	void Start () {
+	}
+	
+	void Update () {
+		transform.Rotate(Vector3.up,20f*Time.deltaTime);
+	}
+}
